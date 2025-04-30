f4c30761add5ca9a908f256d3a4e978200b5b076
wangrl2016 <wangrl2016@gmail.com>
2025-04-30 17:15:09 +0800

    Update indexing.py

--- 01_tensor/2_tensor/indexing.py
+++ 01_tensor/2_tensor/indexing.py
@@ -1,10 +1,14 @@
 import torch
 
 tensor2d = torch.tensor([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-
+assert tensor2d[1][2] == 6
 assert tensor2d[0, 0] == 1
 assert tensor2d[1, 2] == 6
 assert tensor2d[-1, -1] == 9
+
+scalar = torch.tensor([[1]])
+print(scalar)
+assert scalar.item() == 1
 
 tensor2d = torch.tensor([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
 assert (tensor2d[0] == torch.tensor([1, 2, 3])).all()
