2ac7468ff2daed80912ebda247129fcef3601d57
wangrl2016 <wangrl2016@gmail.com>
2025-04-30 12:01:52 +0800

    Update create.py

--- 01_tensor/2_tensor/create.py
+++ 01_tensor/2_tensor/create.py
@@ -1,23 +1,34 @@
 import torch
 import numpy
 
-data = [[1, 2], [3, 4]]
-tensor_from_list = torch.tensor(data)
+python_list = [[1, 2], [3, 4]]
+tensor_from_list = torch.tensor(python_list)
 print('Tensor from list:', tensor_from_list)
 
 numpy_array = numpy.array([[1, 2], [3, 4]])
 tensor_from_array = torch.tensor(numpy_array)
-print(type(tensor_from_array))
 print('Tensor from array:', tensor_from_array)
 
+# avoid a copy
+tensor_from_array = torch.as_tensor(num_array)
+
+device = 'cuda' if torch.cuda.is_available() else 'mps' \
+    if torch.mps.is_available() else 'cpu'
+print(device)
+
+tensor_custom = torch.tensor([1, 2, 3],
+                             dtype=torch.int64,
+                             device=device)
+print(tensor_custom)
+
 tensor_zeros = torch.zeros(size=(3, 3))
-print(tensor_zeros.shape)
-tensor_ones = torch.ones(2, 2)
-tensor_empty = torch.empty(2, 2)
+tensor_ones = torch.ones((2, 2))
+tensor_empty = torch.empty((2, 2))
 print(tensor_zeros)
 print(tensor_ones)
 print(tensor_empty)
 
+torch.manual_seed(47)
 tensor_rand = torch.rand(2, 2)
 tensor_randn = torch.randn(2, 3)
 print(tensor_rand)
@@ -29,12 +40,3 @@ tensor_linspace = torch.linspace(0, 1, steps=5)
 assert torch.allclose(
     tensor_linspace,
     torch.Tensor([0.0000, 0.2500, 0.5000, 0.7500, 1.0000]))
-
-device = 'cuda' if torch.cuda.is_available() else 'mps' \
-    if torch.mps.is_available() else 'cpu'
-print(device)
-
-tensor_custom = torch.tensor([1, 2, 3],
-                             dtype=torch.int64,
-                             device=device)
-print(tensor_custom)
