3aada66c0d9c74c8fa33b87e8560c4922c9c6094
wangrl2016 <wangrl2016@gmail.com>
2025-05-01 18:29:22 +0800

    Add download.py

--- 01_basic/3_represent/image_3d.py
+++ 01_basic/3_represent/image_3d.py
@@ -1,1 +1,11 @@
-import torch
+import sys
+import pathlib
+
+project_root = pathlib.Path(__file__).resolve().parents[2]
+sys.path.append(str(project_root))
+
+from common import download
+
+url = 'https://github.com/artinte/tiny-datasets/raw/develop/mnist.zip'
+
+file_path = download.download(url, sha1_hash='1f2d5ca7b198c0b293014eb59e9064509d7315b1')
---
+++ common/download.py
@@ -0,0 +1,27 @@
+import hashlib
+import os
+import requests
+
+def download(url, folder='./data', sha1_hash=None):
+    '''
+    Download a file to folder and return the local filepath.
+    '''
+    os.makedirs(folder, exist_ok=True)
+    fname = os.path.join(folder, url.split('/')[-1])
+    # check cache
+    if os.path.exists(fname) and sha1_hash:
+        sha1 = hashlib.sha1()
+        with open(fname, 'rb') as f:
+            while True:
+                data = f.read(1024 * 1024)
+                if not data:
+                    break
+                sha1.update(data)
+        if sha1.hexdigest() == sha1_hash:
+            return fname
+    # download
+    print(f'Downloading {fname} from {url} ...')
+    r = requests.get(url, stream=True, verify=True)
+    with open(fname, 'wb') as f:
+        f.write(r.content)
+    return fname
