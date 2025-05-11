a572e81d9ef45f8f8696f8a36e9124b286d36ad2
wangrl2016 <wangrl2016@gmail.com>
2025-05-11 08:41:42 +0800

    Add neural

---
+++ 01_basic/7_nn_stratch/feed_forward.py
@@ -0,0 +1,49 @@
+import numpy
+
+
+def sigmoid(x):
+    return 1 / (1 + numpy.exp(-x))
+
+
+class Neuron:
+    def __init__(self, weights, bias):
+        self.weights = weights
+        self.bias = bias
+
+    def feedforward(self, inputs):
+        total = numpy.dot(self.weights, inputs) + self.bias
+        return sigmoid(total)
+
+
+class OurNeuralNetwork:
+    '''
+    A neural network with:
+      - 2 inputs
+      - a hidden layer with 2 neurons (h1, h2)
+      - an output layer with 1 neuron (o1)
+    Each neuron has the same weights and bias:
+      - w = [0, 1]
+      - b = 0
+    '''
+    def __init__(self):
+        weights = numpy.array([0, 1])
+        bias = 0
+
+        # The Neuron class here is from the previous section.
+        self.h1 = Neuron(weights, bias)
+        self.h2 = Neuron(weights, bias)
+        self.o1 = Neuron(weights, bias)
+
+    def feedforward(self, x):
+        out_h1 = self.h1.feedforward(x)
+        out_h2 = self.h2.feedforward(x)
+        
+        # The inputs for o1 are the outputs from h1 and h2.
+        out_o1 = self.o1.feedforward(numpy.array([out_h1, out_h2]))
+        return out_o1
+
+
+network = OurNeuralNetwork()
+x = numpy.array([2, 3])
+# 0.7216
+print(round(network.feedforward(x), 4))
---
+++ 01_basic/7_nn_stratch/neuron.py
@@ -0,0 +1,29 @@
+import numpy
+
+
+def sigmoid(x):
+    # Our activation function: f(x) = 1 / (1 + e^(-x))
+    return 1 / (1 + numpy.exp(-x))
+
+
+class Neuron:
+    def __init__(self, weights, bias):
+        self.weights = weights
+        self.bias = bias
+
+    def feedforward(self, inputs):
+        # Weight inputs, add bias, then use the activation function
+        total = numpy.matmul(self.weights, inputs) + self.bias
+        return sigmoid(total)
+
+
+# w1 = 0, w2 = 1
+weights = numpy.array([0, 1])
+# b = 4
+bias = 4
+# x1 = 2, x2 = 3
+x = numpy.array([2, 3])
+
+neuron = Neuron(weights, bias)
+# 0.9991
+print('Neuron output is', round(neuron.feedforward(x), 4))
