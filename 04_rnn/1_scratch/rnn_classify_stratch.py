46d341c4bfac757f3b5d39fe9c9fab3538f075f6
wangrl2016 <wangrl2016@gmail.com>
2025-05-22 20:38:32 +0800

    Update index.html

--- 04_rnn/1_scratch/rnn_classify_stratch.py
+++ 04_rnn/1_scratch/rnn_classify_stratch.py
@@ -46,6 +46,11 @@ class RNN:
         return y, h
 
     def backprop(self, dy, learn_rate=2e-2):
+        '''
+        Perform a backward pass of the RNN.
+        - d_y (dL/dy) has shape (output_size, 1).
+        - learn_rate is a float.
+        '''
         # dy (dl/dy) has shape (output_size, 1)
         n = len(self.last_inputs)
 
