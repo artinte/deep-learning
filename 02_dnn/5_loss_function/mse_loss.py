937b2d279e24c0c6cc7e160a76d4b6cc705b8287
wangrl2016 <wangrl2016@gmail.com>
2025-05-29 16:18:29 +0800

    Create mse_loss.py

---
+++ 02_dnn/5_loss_function/mse_loss.py
@@ -0,0 +1,25 @@
+import numpy
+import torch
+
+
+def dervi_mse(y_pred, y_true):
+    return 2 * (y_pred - y_true) / len(y_true)
+
+y_true = numpy.array([1.0, 2.0, 3.0, 4.0, 5.0])
+y_pred = numpy.array([1.2, 1.8, 3.5, 4.1, 5.3])
+
+average_loss = ((y_pred - y_true) ** 2).mean()
+print(average_loss)
+
+dl_dy = dervi_mse(y_pred, y_true)
+print(dl_dy)
+
+y_true = torch.tensor(y_true, requires_grad=False)
+y_pred = torch.tensor(y_pred, requires_grad=True)
+
+# update gradient
+average_loss = torch.nn.MSELoss()(y_pred, y_true)
+print(average_loss.item())
+
+average_loss.backward()
+print(y_pred.grad)
