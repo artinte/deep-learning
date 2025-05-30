c6ac324cef4370ba70cd136b9cc999911c469eb9
wangrl2016 <wangrl2016@gmail.com>
2025-05-30 23:01:29 +0800

    Simple train animation

---
+++ 01_basic/4_principle/simple_train_anim.py
@@ -0,0 +1,41 @@
+import numpy
+from matplotlib import pyplot
+from matplotlib.animation import FuncAnimation
+
+LEARN_RATE = 0.01
+EPOCHS = 5
+rng = numpy.random.default_rng(seed=0)
+random_numbers = rng.standard_normal(size=100)
+x_input_array = numpy.linspace(0, 4, 100)
+y_true_array = 3 * x_input_array + 4 + random_numbers
+param = 1.0
+history = []
+
+for _ in range(EPOCHS):
+    for index, x_input in enumerate(x_input_array):
+        y_pred = param * x_input + 4
+        if y_pred < y_true_array[index]:
+            param += LEARN_RATE
+        else:
+            param -= LEARN_RATE
+        history.append(param)
+
+fig, ax = pyplot.subplots()
+line, = ax.plot(x_input_array, x_input_array + 4)
+text = ax.text(3, 10, 'param: 1.00')
+
+ax.scatter(x_input_array, y_true_array, s=5, c='blue')
+ax.plot(x_input_array, x_input_array + 4, c='red', linestyle='--', label='f(x) = x + 4')
+ax.legend()
+ax.grid(True)
+fig.subplots_adjust(left=0.08, right=0.92, top=0.96, bottom=0.06)
+
+def animate(i):
+    line.set_data(x_input_array, history[i] * x_input_array + 4)
+    text.set_text(f'param: {history[i]:.2f}')
+    return line, text
+
+animation = FuncAnimation(fig, animate, frames=len(history),
+                            interval=40, blit=True, repeat=False)
+animation.save("temp/simple_train_anim.gif", writer="pillow")
+pyplot.show()
