f11469f6d084333ad5833695acfae61ec223d4c2
wangrl2016 <wangrl2016@gmail.com>
2025-06-14 11:27:47 +0800

    Update mnist.py

--- 01_basic/3_represent/image_3d.py
+++ 01_basic/3_represent/image_3d.py
@@ -1,6 +1,5 @@
 import sys
 import pathlib
-import zipfile
 import os
 import random
 from matplotlib import pyplot
@@ -8,27 +7,10 @@ from matplotlib import pyplot
 project_root = pathlib.Path(__file__).resolve().parents[2]
 sys.path.append(str(project_root))
 
-from common import download, mnist
+from common import mnist
 
 
-url = 'https://github.com/artinte/tiny-datasets/raw/develop/mnist.zip'
-
-file_path = download.download(
-    url, sha1_hash='1f2d5ca7b198c0b293014eb59e9064509d7315b1')
-
-extract_dir = os.path.dirname(file_path)
-with zipfile.ZipFile(file_path, 'r') as zip_ref:
-    zip_ref.extractall(extract_dir)
-
-base_dir = os.path.join(extract_dir, 'mnist')
-
-x_train, y_train = mnist.mnist_read(
-    os.path.join(base_dir, 'train-images-idx3-ubyte'),
-    os.path.join(base_dir, 'train-labels-idx1-ubyte'))
-
-x_test, y_test = mnist.mnist_read(
-    os.path.join(base_dir, 't10k-images-idx3-ubyte'),
-    os.path.join(base_dir, 't10k-labels-idx1-ubyte'))
+(x_train, y_train), (x_test, y_test) = mnist.load()
 
 fig = pyplot.figure(figsize=(x_train[0].shape[0] / 10,
                              x_train[0].shape[1] / 10))
--- common/mnist.py
+++ common/mnist.py
@@ -1,6 +1,14 @@
 import array
 import numpy
 import struct
+import pathlib
+import sys
+import os
+import zipfile
+
+sys.path.append(str(pathlib.Path(__file__).resolve().parents[1]))
+
+from common import download
 
 def mnist_read(images_path, labels_path):
     labels = []
@@ -25,3 +33,25 @@ def mnist_read(images_path, labels_path):
         images[j][:] = img
 
     return numpy.array(images), numpy.array(labels)
+
+def load():
+    url = 'https://github.com/artinte/tiny-datasets/raw/develop/mnist.zip'
+
+    file_path = download.download(
+        url, sha1_hash='1f2d5ca7b198c0b293014eb59e9064509d7315b1')
+
+    extract_dir = os.path.dirname(file_path)
+    with zipfile.ZipFile(file_path, 'r') as zip_ref:
+        zip_ref.extractall(extract_dir)
+
+    base_dir = os.path.join(extract_dir, 'mnist')
+
+    x_train, y_train = mnist_read(
+        os.path.join(base_dir, 'train-images-idx3-ubyte'),
+        os.path.join(base_dir, 'train-labels-idx1-ubyte'))
+
+    x_test, y_test = mnist_read(
+        os.path.join(base_dir, 't10k-images-idx3-ubyte'),
+        os.path.join(base_dir, 't10k-labels-idx1-ubyte'))
+
+    return (x_train, y_train), (x_test, y_test)
