00ce5d29a9bffb91acc0c86b6ea4219cd4694a98
wangrl2016 <wangrl2016@gmail.com>
2025-06-19 23:59:53 +0800

    Using tiny model

--- 07_text/3_lang_model/simple_llama.py
+++ 07_text/3_lang_model/simple_llama.py
@@ -1,11 +1,17 @@
 import torch
-from transformers import LlamaForCausalLM, LlamaTokenizer
+from transformers import LlamaForCausalLM, LlamaTokenizer, AutoModelForCausalLM, AutoTokenizer
 
 # model_name = 'NousResearch/Llama-2-7b-chat-hf'
-model_name = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
+# tokenizer = LlamaTokenizer.from_pretrained(model_name)
+# model = LlamaForCausalLM.from_pretrained(model_name, torch_dtype=torch.bfloat16, device_map="auto")
 
-tokenizer = LlamaTokenizer.from_pretrained(model_name)
-model = LlamaForCausalLM.from_pretrained(model_name, torch_dtype=torch.bfloat16, device_map="auto")
+model_name = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    torch_dtype=torch.float32,  # bfloat16 is not supported on Mac CPU
+    device_map={"": "cpu"}      # Run on CPU explicitly
+)
 
 prompt = "<s>[INST] Explain what a black hole is in one sentence. [/INST]"
 
