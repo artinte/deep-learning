19bfd7d065a8982083f06016cccfe088a33b1b14
wangrl2016 <wangrl2016@gmail.com>
2025-07-03 23:07:52 +0800

    Embedding layer

--- 04_rnn/2_word_embed/imdb_classifier_torch.py
+++ 04_rnn/2_word_embed/imdb_classifier_torch.py
@@ -12,17 +12,22 @@ from common import imdb_text
 dataset_dir = imdb_text.get_dataset_dir()
 print(os.listdir(dataset_dir))
 
-train_dir = os.path.join(dataset_dir, 'train')
+train_dir = os.path.join(dataset_dir, "train")
 print(os.listdir(train_dir))
 
-remove_dir = os.path.join(train_dir, 'unsup')
+remove_dir = os.path.join(train_dir, "unsup")
 shutil.rmtree(remove_dir)
 
 # Embed a 1000 word vocabulary into 5 dimensions.
-simple_embedding_layer = torch.nn.Embedding(num_embeddings=1000,
-                                            embedding_dim=5)
+simple_embedding_layer = torch.nn.Embedding(num_embeddings=1000, embedding_dim=5)
 result = simple_embedding_layer(torch.tensor([1, 2, 3]))
+assert result.shape == (3, 5)
 print(result)
+
+embedding_matrix = torch.randn(1000, 5)
+result = embedding_matrix[[1, 2, 3]]
+assert result.shape == (3, 5)
+
 
 result = simple_embedding_layer(torch.tensor([[0, 1, 2], [3, 4, 5]]))
 assert result.shape == (2, 3, 5)
