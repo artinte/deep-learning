ddc42df63866ba47394cb64073b12c4c2cdb659d
wangrl2016 <wangrl2016@gmail.com>
2025-07-05 20:03:27 +0800

    Additive attention

---
+++ 05_transformer/1_attention/additive_attention.py
@@ -0,0 +1,43 @@
+import torch
+
+from util import masked_softmax, show_heatmaps
+
+
+class AdditiveAttention(torch.nn.Module):  # @save
+    """Additive attention."""
+
+    def __init__(self, num_hiddens, dropout, **kwargs):
+        super(AdditiveAttention, self).__init__(**kwargs)
+        self.W_k = torch.nn.LazyLinear(num_hiddens, bias=False)
+        self.W_q = torch.nn.LazyLinear(num_hiddens, bias=False)
+        self.w_v = torch.nn.LazyLinear(1, bias=False)
+        self.dropout = torch.nn.Dropout(dropout)
+
+    def forward(self, queries, keys, values, valid_lens):
+        queries, keys = self.W_q(queries), self.W_k(keys)
+        # After dimension expansion, shape of queries: (batch_size, no. of
+        # queries, 1, num_hiddens) and shape of keys: (batch_size, 1, no. of
+        # key-value pairs, num_hiddens). Sum them up with broadcasting
+        features = queries.unsqueeze(2) + keys.unsqueeze(1)
+        features = torch.tanh(features)
+        # There is only one output of self.w_v, so we remove the last
+        # one-dimensional entry from the shape. Shape of scores: (batch_size,
+        # no. of queries, no. of key-value pairs)
+        scores = self.w_v(features).squeeze(-1)
+        self.attention_weights = masked_softmax(scores, valid_lens)
+        # Shape of values: (batch_size, no. of key-value pairs, value
+        # dimension)
+        return torch.bmm(self.dropout(self.attention_weights), values)
+
+
+queries = torch.normal(0, 1, (2, 1, 20))
+keys = torch.normal(0, 1, (2, 10, 2))
+values = torch.normal(0, 1, (2, 10, 4))
+valid_lens = torch.tensor([2, 6])
+
+attention = AdditiveAttention(num_hiddens=8, dropout=0.1)
+attention.eval()
+assert attention(queries, keys, values, valid_lens).shape == (2, 1, 4)
+
+show_heatmaps(attention.attention_weights.reshape((1, 1, 2, 10)),
+              xlabel='Keys', ylabel='Queries')
--- 05_transformer/1_attention/dot_prod_attention.py
+++ 05_transformer/1_attention/dot_prod_attention.py
@@ -1,30 +1,6 @@
 import math
 import torch
-from util import show_heatmaps
-
-
-def masked_softmax(X, valid_lens):
-    """Perform softmax operation by masking elements on the last axis."""
-    # X: 3D tensor, valid_lens: 1D or 2D tensor
-    def _sequence_mask(X, valid_len, value=0):
-        maxlen = X.size(1)
-        mask = torch.arange((maxlen), dtype=torch.float32,
-                            device=X.device)[None, :] < valid_len[:, None]
-        X[~mask] = value
-        return X
-
-    if valid_lens is None:
-        return torch.nn.functional.softmax(X, dim=-1)
-    else:
-        shape = X.shape
-        if valid_lens.dim() == 1:
-            valid_lens = torch.repeat_interleave(valid_lens, shape[1])
-        else:
-            valid_lens = valid_lens.reshape(-1)
-        # On the last axis, replace masked elements with a very large negative
-        # value, whose exponentiation outputs 0
-        X = _sequence_mask(X.reshape(-1, shape[-1]), valid_lens, value=-1e6)
-        return torch.nn.functional.softmax(X.reshape(shape), dim=-1)
+from util import masked_softmax, show_heatmaps
 
 
 print(masked_softmax(torch.rand(2, 2, 4), torch.tensor([2, 3])))
