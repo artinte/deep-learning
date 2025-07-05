ac0cfefc82d5e5e8bf5245cd0d067770660f61ec
wangrl2016 <wangrl2016@gmail.com>
2025-07-05 14:15:31 +0800

    Add resnet_torch.py

--- 03_cnn/3_resnet/resnet_torch.py
+++ 03_cnn/3_resnet/resnet_torch.py
@@ -1,15 +1,27 @@
+import pathlib
+import sys
 import torch
+import torchvision
+from matplotlib import pyplot
+from PIL import Image
 
-class ResidualBlock(torch.nn.Module):
+project_root = pathlib.Path(__file__).resolve().parents[2]
+sys.path.append(str(project_root))
+
+from common import download
+
+class BasicBlock(torch.nn.Module):
     expansion = 1
 
-    def __init__(self, in_channels, out_channels, stride=1, downsample=None):
-        super(ResidualBlock, self).__init__()
-        self.conv1 = torch.nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.bn1 = torch.nn.BatchNorm2d(out_channels)
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super().__init__()
+        self.conv1 = torch.nn.Conv2d(inplanes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = torch.nn.BatchNorm2d(planes)
         self.relu = torch.nn.ReLU(inplace=True)
-        self.conv2 = torch.nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn2 = torch.nn.BatchNorm2d(out_channels)
+        
+        self.conv2 = torch.nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = torch.nn.BatchNorm2d(planes)
+        
         self.downsample = downsample
 
     def forward(self, x):
@@ -18,15 +30,119 @@ class ResidualBlock(torch.nn.Module):
         out = self.conv1(x)
         out = self.bn1(out)
         out = self.relu(out)
-
+        
         out = self.conv2(out)
         out = self.bn2(out)
-
-        if self.downsample:
+        
+        if self.downsample is not None:
             identity = self.downsample(x)
-
+        
         out += identity
         out = self.relu(out)
-
+        
         return out
 
+class ResNet(torch.nn.Module):
+    def __init__(self, block, layers, num_classes=1000):
+        super().__init__()
+        self.inplanes = 64
+        
+        self.conv1 = torch.nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.bn1 = torch.nn.BatchNorm2d(64)
+        self.relu = torch.nn.ReLU(inplace=True)
+        
+        self.maxpool = torch.nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        
+        self.layer1 = self._make_layer(block, 64,  layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+        
+        self.avgpool = torch.nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = torch.nn.Linear(512 * block.expansion, num_classes)
+        
+    def _make_layer(self, block, planes, blocks, stride=1):
+        downsample = None
+        
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = torch.nn.Sequential(
+                torch.nn.Conv2d(self.inplanes, planes * block.expansion,
+                          kernel_size=1, stride=stride, bias=False),
+                torch.nn.BatchNorm2d(planes * block.expansion),
+            )
+            
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample))
+        self.inplanes = planes * block.expansion
+        
+        for _ in range(1, blocks):
+            layers.append(block(self.inplanes, planes))
+        
+        return torch.nn.Sequential(*layers)
+    
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        
+        x = self.maxpool(x)
+        
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.fc(x)
+        
+        return x
+
+def resnet34(pretrained=False, **kwargs):
+    model = ResNet(BasicBlock, [3, 4, 6, 3], **kwargs)
+    if pretrained:
+        state_dict = torch.hub.load_state_dict_from_url(
+            'https://download.pytorch.org/models/resnet34-333f7ec4.pth',
+            progress=True
+        )
+        model.load_state_dict(state_dict)
+    return model
+
+
+resnet = resnet34(pretrained=True)
+resnet.eval()
+
+img_path = download.download('https://img-datasets.s3.amazonaws.com/cat.jpg')
+image = Image.open(img_path).convert('RGB')
+
+preprocess = torchvision.transforms.Compose([
+    torchvision.transforms.Resize(256),
+    torchvision.transforms.CenterCrop(224),
+    torchvision.transforms.ToTensor(),
+    torchvision.transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.229, 0.224, 0.225])
+])
+
+input_tensor = preprocess(image).unsqueeze(0)  # [1, 3, 224, 224]
+print(input_tensor.shape)
+
+pyplot.axis('off')
+pyplot.imshow(image)
+pyplot.subplots_adjust(left=0, right=1.0, top=1.0, bottom=0)
+pyplot.show()
+
+imagenet_labels_path = download.download(
+    "https://raw.githubusercontent.com/pytorch/hub/master/imagenet_classes.txt")
+with open(imagenet_labels_path) as f:
+    labels = [line.strip() for line in f.readlines()]
+
+with torch.no_grad():
+    output = resnet(input_tensor)
+    probs = torch.nn.functional.softmax(output[0], dim=0)
+    top3_prob, top3_idx = torch.topk(probs, 3)
+
+for i in range(3):
+    class_idx = top3_idx[i].item()
+    prob = top3_prob[i].item()
+    label = labels[class_idx]
+    print(f"Top {i+1}: {label} ({prob:.4f})")
