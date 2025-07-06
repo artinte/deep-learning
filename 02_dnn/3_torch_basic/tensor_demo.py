9d14d67ba8b29d03edefed26c0c283923590fe83
wangrl2016 <wangrl2016@gmail.com>
2025-07-06 13:41:06 +0800

    Tensor operation

--- 02_dnn/3_torch_basic/tensor_demo.py
+++ 02_dnn/3_torch_basic/tensor_demo.py
@@ -25,3 +25,14 @@ zeros_tensor = torch.zeros(shape)
 print(f"Random Tensor: \n {rand_tensor} \n")
 print(f"Ones Tensor: \n {ones_tensor} \n")
 print(f"Zeros Tensor: \n {zeros_tensor}")
+
+tensor = torch.rand(3,4)
+
+print(f"Shape of tensor: {tensor.shape}")
+print(f"Datatype of tensor: {tensor.dtype}")
+print(f"Device tensor is stored on: {tensor.device}")
+
+# We move our tensor to the current accelerator if available
+if torch.accelerator.is_available():
+    tensor = tensor.to(torch.accelerator.current_accelerator())
+print(f"Device tensor is stored on: {tensor.device}")
