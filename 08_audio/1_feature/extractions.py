0d129d35059e66354a8ae6c62331825d848e96de
wangrl2016 <wangrl2016@gmail.com>
2025-07-06 16:57:54 +0800

    Feature extractions

---
+++ 08_audio/1_feature/extractions.py
@@ -0,0 +1,67 @@
+import librosa
+import torch
+import torchaudio
+from matplotlib import pyplot
+
+print(torch.__version__)
+print(torchaudio.__version__)
+
+torch.random.manual_seed(0)
+
+SAMPLE_SPEECH = torchaudio.utils.download_asset(
+    "tutorial-assets/Lab41-SRI-VOiCES-src-sp0307-ch127535-sg0042.wav"
+)
+
+
+def plot_waveform(waveform, sr, title="Waveform", ax=None):
+    waveform = waveform.numpy()
+
+    num_channels, num_frames = waveform.shape
+    time_axis = torch.arange(0, num_frames) / sr
+
+    if ax is None:
+        _, ax = pyplot.subplots(num_channels, 1)
+    ax.plot(time_axis, waveform[0], linewidth=1)
+    ax.grid(True)
+    ax.set_xlim([0, time_axis[-1]])
+    ax.set_title(title)
+
+
+def plot_spectrogram(specgram, title=None, ylabel="freq_bin", ax=None):
+    if ax is None:
+        _, ax = pyplot.subplots(1, 1)
+    if title is not None:
+        ax.set_title(title)
+    ax.set_ylabel(ylabel)
+    ax.imshow(
+        librosa.power_to_db(specgram),
+        origin="lower",
+        aspect="auto",
+        interpolation="nearest",
+    )
+
+
+def plot_fbank(fbank, title=None):
+    fig, axs = pyplot.subplots(1, 1)
+    axs.set_title(title or "Filter bank")
+    axs.imshow(fbank, aspect="auto")
+    axs.set_ylabel("frequency bin")
+    axs.set_xlabel("mel bin")
+
+
+# load audio
+SPEECH_WAVEFORM, SAMPLE_RATE = torchaudio.load(SAMPLE_SPEECH)
+
+# define transform
+spectrogram = torchaudio.transforms.Spectrogram(n_fft=512)
+
+# perform transform
+spec = spectrogram(SPEECH_WAVEFORM)
+
+fig, axs = pyplot.subplots(2, 1)
+plot_waveform(SPEECH_WAVEFORM, SAMPLE_RATE, title="Original waveform", ax=axs[0])
+plot_spectrogram(spec[0], title="spectrogram", ax=axs[1])
+fig.tight_layout()
+pyplot.show()
+
+
