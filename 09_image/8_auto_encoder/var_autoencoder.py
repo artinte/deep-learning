106066d94c15c717905fd200b7f69b989f2b2ede
wangrl2016 <wangrl2016@gmail.com>
2025-07-13 22:44:56 +0800

    Add var_autoencoder.py

---
+++ 09_image/8_auto_encoder/var_autoencoder.py
@@ -0,0 +1,30 @@
+import torch
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.1307,), (0.3081,))
+])
+
+train_dataset = datasets.MNIST(
+    root='./data',
+    train=True,
+    transform=transform,
+    download=True
+)
+
+test_dataset = datasets.MNIST(
+    root='./data',
+    train=False,
+    transform=transform,
+    download=True
+)
+
+train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=1000, shuffle=False)
+
+for images, labels in train_loader:
+    print(images.shape)  # [64, 1, 28, 28]
+    print(labels.shape)  # [64]
+    break
