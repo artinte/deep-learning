a23591562570116380c258c39aead0ed140ab320
wangrl2016 <wangrl2016@gmail.com>
2025-07-19 10:58:10 +0800

    Point data

--- 01_basic/3_represent/features_1d.py
+++ 01_basic/3_represent/features_1d.py
@@ -2,13 +2,19 @@ import torch
 import numpy
 from matplotlib import pyplot
 
+point_sample = torch.tensor([0.5, 1])
+
+
 rng = numpy.random.default_rng(seed=0)
 random_numbers = rng.standard_normal(size=100)
 x_input_array = numpy.linspace(0, 4, 100)
 y_true_array = 3 * x_input_array + 4 + random_numbers
 
-pyplot.scatter(x_input_array, y_true_array, s=5, c='blue')
-pyplot.plot(x_input_array, 3 * x_input_array + 4, c='red', label='f(x) = 3x + 4')
+point_dataset = numpy.stack((x_input_array, y_true_array), axis=-1)
+assert point_dataset.shape == (100, 2)
+
+pyplot.scatter(x_input_array, y_true_array, s=5, c="blue")
+pyplot.plot(x_input_array, 3 * x_input_array + 4, c="red", label="f(x) = 3x + 4")
 pyplot.legend()
 pyplot.grid(True)
 pyplot.subplots_adjust(left=0.08, right=0.92, top=0.96, bottom=0.06)
