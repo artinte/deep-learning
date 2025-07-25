ea85802f488533a58bef19012508b5b72b42022e
wangrl2016 <wangrl2016@gmail.com>
2025-07-25 23:22:10 +0800

    Update basics

---
+++ 02_dnn/3_torch_basic/simple_auto_diff.py
@@ -0,0 +1,12 @@
+import torch
+
+# input tensor
+x = torch.ones(5)
+# expected output
+y = torch.zeros(3)
+w = torch.randn(5, 3, requires_grad=True)
+b = torch.randn(3, requires_grad=True)
+z = torch.matmul(x, w) + b
+loss = torch.nn.functional.binary_cross_entropy_with_logits(z, y)
+
+print(loss)
