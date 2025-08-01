180cbe23e70aa15ee6da80ac15a97e68901e6e1f
wangrl2016 <wangrl2016@gmail.com>
2025-08-01 21:54:53 +0800

    Train model

--- 09_image/2_transfer_learning/simple_transfer.py
+++ 09_image/2_transfer_learning/simple_transfer.py
@@ -58,98 +58,98 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
     since = time.time()
 
     # Create a temporary directory to save training checkpoints
-    with tempfile.TemporaryDirectory() as tempdir:
-        best_model_params_path = os.path.join(tempdir, 'best_model_params.pt')
+    tempdir = 'temp/'
+    os.makedirs(tempdir, exist_ok=True)
+    best_model_params_path = os.path.join(tempdir, 'best_model_params.pt')
 
-        torch.save(model.state_dict(), best_model_params_path)
-        best_acc = 0.0
+    torch.save(model.state_dict(), best_model_params_path)
+    best_acc = 0.0
 
-        for epoch in range(num_epochs):
-            print(f'Epoch {epoch}/{num_epochs - 1}')
-            print('-' * 10)
+    for epoch in range(num_epochs):
+        print(f'Epoch {epoch}/{num_epochs - 1}')
+        print('-' * 10)
 
-            # Each epoch has a training and validation phase
-            for phase in ['train', 'val']:
-                if phase == 'train':
-                    model.train()  # Set model to training mode
-                else:
-                    model.eval()   # Set model to evaluate mode
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                model.train()  # Set model to training mode
+            else:
+                model.eval()   # Set model to evaluate mode
 
-                running_loss = 0.0
-                running_corrects = 0
+            running_loss = 0.0
+            running_corrects = 0
 
-                # Iterate over data.
-                for inputs, labels in dataloaders[phase]:
-                    inputs = inputs.to(device)
-                    labels = labels.to(device)
+            # Iterate over data.
+            for inputs, labels in dataloaders[phase]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
 
-                    # zero the parameter gradients
-                    optimizer.zero_grad()
+                # zero the parameter gradients
+                optimizer.zero_grad()
 
-                    # forward
-                    # track history if only in train
-                    with torch.set_grad_enabled(phase == 'train'):
-                        outputs = model(inputs)
-                        _, preds = torch.max(outputs, 1)
-                        loss = criterion(outputs, labels)
+                # forward
+                # track history if only in train
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    loss = criterion(outputs, labels)
 
-                        # backward + optimize only if in training phase
-                        if phase == 'train':
-                            loss.backward()
-                            optimizer.step()
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
 
-                    # statistics
-                    running_loss += loss.item() * inputs.size(0)
-                    running_corrects += torch.sum(preds == labels.data)
-                if phase == 'train':
-                    scheduler.step()
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+            if phase == 'train':
+                scheduler.step()
 
-                epoch_loss = running_loss / dataset_sizes[phase]
-                epoch_acc = running_corrects.double() / dataset_sizes[phase]
+            epoch_loss = running_loss / dataset_sizes[phase]
+            epoch_acc = running_corrects.double() / dataset_sizes[phase]
 
-                print(f'{phase} Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}')
+            print(f'{phase} Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}')
 
-                # deep copy the model
-                if phase == 'val' and epoch_acc > best_acc:
-                    best_acc = epoch_acc
-                    torch.save(model.state_dict(), best_model_params_path)
+            # deep copy the model
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                torch.save(model.state_dict(), best_model_params_path)
 
-            print()
+        print()
 
-        time_elapsed = time.time() - since
-        print(f'Training complete in {time_elapsed // 60:.0f}m {time_elapsed % 60:.0f}s')
-        print(f'Best val Acc: {best_acc:4f}')
+    time_elapsed = time.time() - since
+    print(f'Training complete in {time_elapsed // 60:.0f}m {time_elapsed % 60:.0f}s')
+    print(f'Best val Acc: {best_acc:4f}')
 
-        # load best model weights
-        model.load_state_dict(torch.load(best_model_params_path, weights_only=True))
+    # load best model weights
+    model.load_state_dict(torch.load(best_model_params_path, weights_only=True))
     return model
 
 
-def visualize_model(model, num_images=6):
+def visualize_model(model):
     was_training = model.training
     model.eval()
-    images_so_far = 0
-    fig = pyplot.figure()
 
     with torch.no_grad():
-        for i, (inputs, labels) in enumerate(dataloaders['val']):
-            inputs = inputs.to(device)
-            labels = labels.to(device)
+        inputs, labels = next(iter(dataloaders['val']))
+        inputs = inputs.to(device)
+        labels = labels.to(device)
 
-            outputs = model(inputs)
-            _, preds = torch.max(outputs, 1)
+        outputs = model(inputs)
+        _, preds = torch.max(outputs, 1)
 
-            for j in range(inputs.size()[0]):
-                images_so_far += 1
-                ax = pyplot.subplot(num_images//2, 2, images_so_far)
-                ax.axis('off')
-                ax.set_title(f'predicted: {class_names[preds[j]]}')
-                imshow(inputs.cpu().data[j])
+        _, axes = pyplot.subplots(3, 2)
+        axes = axes.flatten()
+        for j in range(6):
+            ax = axes[j]
+            ax.axis('off')
+            ax.set_title(f'predicted: {class_names[preds[j]]}')
+            imshow(inputs.cpu()[j], ax=ax)
 
-                if images_so_far == num_images:
-                    model.train(mode=was_training)
-                    return
-        model.train(mode=was_training)
+    # Restore model to original training state     
+    model.train(mode=was_training)
+    pyplot.tight_layout()
+    pyplot.show()
 
 
 def visualize_model_predictions(model,img_path):
@@ -229,6 +229,8 @@ if __name__ == "__main__":
     out = torchvision.utils.make_grid(inputs)
     imshow(out, title=[class_names[x] for x in classes])
 
+
+    print("Fine-tuning the convnet...")
     model_ft = models.resnet18(weights='IMAGENET1K_V1')
     num_ftrs = model_ft.fc.in_features
     # Here the size of each output sample is set to 2.
@@ -247,8 +249,8 @@ if __name__ == "__main__":
     
     model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler,
                        num_epochs=25)
-    
     visualize_model(model_ft)
+
 
     model_conv = torchvision.models.resnet18(weights='IMAGENET1K_V1')
     for param in model_conv.parameters():
