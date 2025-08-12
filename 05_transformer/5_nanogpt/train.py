a73db6a2daf493803aef5d35177a411fd21194ee
wangrl2016 <wangrl2016@gmail.com>
2025-08-12 11:27:36 +0800

    Update train.py

--- 05_transformer/5_nanogpt/train.py
+++ 05_transformer/5_nanogpt/train.py
@@ -3,3 +3,44 @@ import time
 import math
 import pickle
 
+import numpy
+import torch
+
+
+"""
+This training script can be run both on a single gpu in debug mode,
+and also in a larger training run with distributed data parallel (ddp).
+
+To run on a single GPU, example:
+$ python train.py --batch_size=32
+
+"""
+
+out_dir = 'out'
+always_save_checkpoint = True # if True, always save a checkpoint after each eval
+
+
+# data
+dataset = 'openwebtext'
+
+
+# model parameters
+n_layer = 12
+n_head = 12
+n_embd = 768
+dropout = 0.0 # for pretraining 0 is good, for finetuning try 0.1+
+bias = False # do we use bias inside LayerNorm and Linear layers?
+
+# adamw optimizer
+learning_rate = 6e-4 # max learning rate (0.0006)
+max_iters = 600000 # total number of training iterations
+weight_decay = 1e-1
+beta1 = 0.9
+beta2 = 0.95
+
+# system
+device = 'cuda' # examples: 'cpu', 'cuda', 'cuda:0', 'cuda:1' etc., or try 'mps' on macbooks
+# 'float32', 'bfloat16', or 'float16', the latter will auto implement a GradScaler
+dtype = 'bfloat16' if torch.cuda.is_available() and torch.cuda.is_bf16_supported() else 'float16'
+
+
