96b2d1caebc450309561d10b463cbd2d58775af0
wangrl2016 <wangrl2016@gmail.com>
2025-08-16 11:27:44 +0800

    Scaled dot product attention

--- 05_transformer/5_nanogpt/simple_demo.py
+++ 05_transformer/5_nanogpt/simple_demo.py
@@ -14,3 +14,12 @@ q, k, v = result.split(n_embed, dim=2)
 print('Query shape:', q.shape)
 print('Key shape:', k.shape)
 print('Value shape:', v.shape)
+
+
+n_head = 2
+q = q.view(batch_size, seq_length, n_head, n_embed // n_head).transpose(1, 2)
+k = k.view(batch_size, seq_length, n_head, n_embed // n_head).transpose(1, 2)
+v = v.view(batch_size, seq_length, n_head, n_embed // n_head).transpose(1, 2)
+print('Query divided shape:', q.shape)
+print('Key divided shape', k.shape)
+print('Value divied shape', v.shape)
