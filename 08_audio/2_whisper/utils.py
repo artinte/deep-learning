a5211fcb04c5d37220a48c51215e9819f20bc340
wangrl2016 <wangrl2016@gmail.com>
2025-08-17 15:07:47 +0800

    Write whisper

---
+++ 08_audio/2_whisper/utils.py
@@ -0,0 +1,37 @@
+import string
+import sys
+
+system_encoding = sys.getdefaultencoding()
+
+if system_encoding != "utf-8":
+    # replaces any character not representable using the system default encoding with an '?',
+    # avoiding UnicodeEncodeError
+    def make_safe(string):
+        return string.encode(system_encoding, errors="replace").decode(system_encoding)
+
+else:
+
+    def make_safe(string):
+        # utf-8 can encode any Unicode code point, so no need to do the round-trip encoding
+        return string
+
+
+def exact_div(x, y):
+    assert x % y == 0
+    return x // y
+
+
+def str2bool(string):
+    str2val = {"True": True, "False": False}
+    if string in str2val:
+        return str2val[string]
+    else:
+        return ValueError(f"Expected one of {set(str2val.keys())}, got {string}")
+
+
+def optional_int(string):
+    return None if string == "None" else int(string)
+
+
+def optional_float(string):
+    return None if string == "None" else float(string)
