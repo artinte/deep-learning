dee2a597e563b84edcc204226a40941fa8692a2f
wangrl2016 <wangrl2016@gmail.com>
2025-08-19 21:36:11 +0800

    Multi head attention

--- 08_audio/2_whisper/core/model.py
+++ 08_audio/2_whisper/core/model.py
@@ -1,4 +1,8 @@
+import numpy
 import torch
+from contextlib import contextmanager
+from dataclasses import dataclass
+from typing import Dict, Iterable, Optional, Tuple
 
 try:
     from torch.nn.functional import scaled_dot_product_attention
@@ -7,6 +11,12 @@ try:
 except (ImportError, RuntimeError, OSError):
     scaled_dot_product_attention = None
     SDPA_AVAILABLE = False
+
+if SDPA_AVAILABLE:
+    print("Scaled Dot-Product Attention is available and ready for use.")
+else:
+    print("Scaled Dot-Product Attention is not available. Falling back to a standard implementation.")
+
 
 @dataclass
 class ModelDimensions:
@@ -21,22 +31,25 @@ class ModelDimensions:
     n_text_head: int
     n_text_layer: int
 
-class LayerNorm(nn.LayerNorm):
-    def forward(self, x: Tensor) -> Tensor:
+
+class LayerNorm(torch.nn.LayerNorm):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         return super().forward(x.float()).type(x.dtype)
 
-class Linear(nn.Linear):
-    def forward(self, x: Tensor) -> Tensor:
-        return F.linear(
+
+class Linear(torch.nn.Linear):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return torch.nn.functional.linear(
             x,
             self.weight.to(x.dtype),
             None if self.bias is None else self.bias.to(x.dtype),
         )
 
-class Conv1d(nn.Conv1d):
+
+class Conv1d(torch.nn.Conv1d):
     def _conv_forward(
-        self, x: Tensor, weight: Tensor, bias: Optional[Tensor]
-    ) -> Tensor:
+        self, x: torch.Tensor, weight: torch.Tensor, bias: Optional[torch.Tensor]
+    ) -> torch.Tensor:
         return super()._conv_forward(
             x, weight.to(x.dtype), None if bias is None else bias.to(x.dtype)
         )
@@ -45,9 +58,11 @@ class Conv1d(nn.Conv1d):
 def sinusoids(length, channels, max_timescale=10000):
     """Returns sinusoids for positional embedding"""
     assert channels % 2 == 0
-    log_timescale_increment = np.log(max_timescale) / (channels // 2 - 1)
-    inv_timescales = torch.exp(-log_timescale_increment * torch.arange(channels // 2))
-    scaled_time = torch.arange(length)[:, np.newaxis] * inv_timescales[np.newaxis, :]
+    log_timescale_increment = numpy.log(max_timescale) / (channels // 2 - 1)
+    inv_timescales = torch.exp(-log_timescale_increment *
+                               torch.arange(channels // 2))
+    scaled_time = torch.arange(
+        length)[:, numpy.newaxis] * inv_timescales[numpy.newaxis, :]
     return torch.cat([torch.sin(scaled_time), torch.cos(scaled_time)], dim=1)
 
 
@@ -61,19 +76,96 @@ def disable_sdpa():
         MultiHeadAttention.use_sdpa = prev_state
 
 
-class MultiHeadAttention(nn.Module):
+class MultiHeadAttention(torch.nn.Module):
+    # Scaled Dot-Product Attention
     use_sdpa = True
 
-    def __init__(self, n_state: int, n_head: int):
+    def __init__(self, d_model: int, n_head: int):
         super().__init__()
         self.n_head = n_head
+        self.query = torch.nn.Linear(d_model, d_model)
+        self.key = torch.nn.Linear(d_model, d_model, bias=False)
+        self.value = torch.nn.Linear(d_model, d_model)
+        self.out = torch.nn.Linear(d_model, d_model)
 
     def forward(
         self,
-        x: Tensor,
-        xa: Optional[Tensor] = None,
-        mask: Optional[Tensor] = None,
+        x: torch.Tensor,
+        xa: Optional[torch.Tensor] = None,
+        mask: Optional[torch.Tensor] = None,
         kv_cache: Optional[dict] = None,
     ):
         q = self.query(x)
+
+        if kv_cache is None or xa is None or self.key not in kv_cache:
+            # hooks, if installed (i.e. kv_cache is not None), will prepend the cached kv tensors;
+            # otherwise, perform key/value projections for self- or cross-attention as usual.
+            k = self.key(x if xa is None else xa)
+            v = self.value(x if xa is None else xa)
+        else:
+            # for cross-attention, calculate keys and values once and reuse in subsequent calls.
+            k = kv_cache[self.key]
+            v = kv_cache[self.value]
+
+        wv, attn_scores = self.qkv_attention(q, k, v, mask)
+        return self.out(wv), attn_scores
+
+    def qkv_attention(
+        self, q: torch.Tensor, k: torch.Tensor, v: torch.Tensor, mask: Optional[torch.Tensor] = None
+    ) -> Tuple[torch.Tensor, Optional[torch.Tensor]]:
+        n_batch, n_ctx, n_state = q.shape
+        scale = (n_state // self.n_head) ** -0.25
+        q = q.view(*q.shape[:2], self.n_head, -1).permute(0, 2, 1, 3)
+        k = k.view(*k.shape[:2], self.n_head, -1).permute(0, 2, 1, 3)
+        v = v.view(*v.shape[:2], self.n_head, -1).permute(0, 2, 1, 3)
+
+        if SDPA_AVAILABLE and MultiHeadAttention.use_sdpa:
+            a = scaled_dot_product_attention(
+                q, k, v, is_causal=mask is not None and n_ctx > 1
+            )
+            out = a.permute(0, 2, 1, 3).flatten(start_dim=2)
+            attn_scores = None
+        else:
+            attn_scores = (q * scale) @ (k * scale).transpose(-1, -2)
+            if mask is not None:
+                attn_scores = attn_scores + mask[:n_ctx, :n_ctx]
+            attn_scores = attn_scores.float()
+
+            w = torch.nn.functional.softmax(attn_scores, dim=-1).to(q.dtype)
+            out = (w @ v).permute(0, 2, 1, 3).flatten(start_dim=2)
+            attn_scores = attn_scores.detach()
+
+        return out, attn_scores
+
+
+class ResidualAttentionBlock(torch.nn.Module):
+    def __init__(self, n_state: int, n_head: int, cross_attention: bool = False):
+        super().__init__()
+        
+        self.attn = MultiHeadAttention(n_state, n_head)
+        self.attn_ln = LayerNorm(n_state)
+        
+        self.cross_attn = (
+            MultiHeadAttention(n_state, n_head) if cross_attention else None
+        )
+        self.cross_attn_ln = LayerNorm(n_state) if cross_attention else None
+        
+        n_mlp = n_state * 4
+        self.mlp = torch.nn.Sequential(
+            Linear(n_state, n_mlp), torch.nn.GELU(), Linear(n_mlp, n_state)
+        )
+        self.mlp_ln = LayerNorm(n_state)
+        
+    def forward(
+        self,
+        x: torch.Tensor,
+        xa: Optional[torch.Tensor] = None,
+        mask: Optional[torch.Tensor] = None,
+        kv_cache: Optional[dict] = None,
+    ):
+        x = x + self.attn(self.attn_ln(x), mask=mask, kv_cache=kv_cache)[0]
+        if self.cross_attn:
+            x = x + self.cross_attn(self.cross_attn_ln(x), xa, kv_cache=kv_cache)[0]
+        x = x + self.mlp(self.mlp_ln(x))
+        return x
 
