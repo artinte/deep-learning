595365d9e38f7d7083690856208e0ea2a7588f8e
wangrl2016 <wangrl2016@gmail.com>
2025-08-29 15:03:41 +0800

    Update train.py

--- 05_transformer/3_transformer_api/train.py
+++ 05_transformer/3_transformer_api/train.py
@@ -7,6 +7,8 @@ def train(model, train_dataloader, optimizer, criterion):
     Args:
         model (torch.nn.Module): The Transformer model.
         train_dataloader (torch.utils.data.DataLoader): The training data loader.
+            Outputs (src, tgt, src_key_padding_mask, tgt_key_padding_mask),
+            where 0 in masks represents padding tokens, and all tensors are already on the target device.
         optimizer (torch.optim.Optimizer): The optimizer for training.
         criterion (torch.nn.modules.loss._Loss): The loss function.
     """
@@ -21,20 +23,20 @@ def train(model, train_dataloader, optimizer, criterion):
         src_key_padding_mask = src_key_padding_mask == 0
         tgt_key_padding_mask = tgt_key_padding_mask == 0
 
-        # [batch_size, tgt_seq_len, vocab_size]
+        # [batch_size, tgt_seq_len - 1, tgt_vocab_size]
         logits = model.forward(
             src, tgt_input, src_key_padding_mask, tgt_key_padding_mask
         )
         output = logits.reshape(-1, logits.shape[-1])
 
-        # [batch, seq_len] -> [batch x seq_len]
+        # [batch_size, tgt_seq_len - 1] -> [batch_size x (tgt_seq_len - 1)]
         tgt_out = tgt[:, 1:].reshape(-1)
 
         # backpropagation
         optimizer.zero_grad()
         loss = criterion(output, tgt_out)
         loss.backward()
-        torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+        torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0, norm_type=2)
         optimizer.step()
 
         total_loss += loss.item()
