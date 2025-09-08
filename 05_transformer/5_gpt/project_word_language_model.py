89de27f116a31fe2a687f535a46048d295eece5a
wangrl2016 <wangrl2016@gmail.com>
2025-09-08 22:16:52 +0800

    Create project_word_language_model.py

---
+++ 05_transformer/5_gpt/project_word_language_model.py
@@ -0,0 +1,52 @@
+import torch
+
+class Config:
+  # size of word embeddings
+  emsize=200
+  # the number of heads in the encoder/decoder of the transformer model
+  nhead=2
+  # number of hidden units per layer
+  nhid=200
+  # number of layers
+  nlayers
+
+
+torch.manual_seed(0)
+
+if torch.accelerator.is_available():
+    device = torch.accelerator.current_accelerator()
+else:
+    device = torch.device("cpu")
+
+print("Using device:", device)
+
+# load data
+corpus = data.Corpus(args.data)
+
+# Starting from sequential data, batchify arranges the dataset into columns.
+# For instance, with the alphabet as the sequence and batch size 4, we'd get
+# ┌ a g m s ┐
+# │ b h n t │
+# │ c i o u │
+# │ d j p v │
+# │ e k q w │
+# └ f l r x ┘.
+# These columns are treated as independent by the model, which means that the
+# dependence of e. g. 'g' on 'f' can not be learned, but allows more efficient
+# batch processing.
+
+def batchify(data, bsz):
+    # Work out how cleanly we can divide the dataset into bsz parts.
+    nbatch = data.size(0) // bsz
+    # Trim off any extra elements that wouldn't cleanly fit (remainders).
+    data = data.narrow(0, 0, nbatch * bsz)
+    # Evenly divide the data across the bsz batches.
+    data = data.view(bsz, -1).t().contiguous()
+    return data.to(device)
+
+eval_batch_size = 10
+train_data = batchify(corpus.train, args.batch_size)
+val_data = batchify(corpus.valid, eval_batch_size)
+test_data = batchify(corpus.test, eval_batch_size)
+
+# build the model
