65a39eaef29b9654f247298cd9a9fc5172721262
wangrl2016 <wangrl2016@gmail.com>
2025-09-15 15:41:07 +0800

    Create demo_diffusion_training.py

---
+++ 06_diffusion/4_scratch/demo_diffusion_training.py
@@ -0,0 +1,20 @@
+import torch
+import functools
+
+
+device = 'cuda'  # @param ['cuda', 'cpu'] {'type':'string'}
+
+def marginal_prob_std(t, sigma):
+    """Compute the mean and standard deviation of $p_{0t}(x(t) | x(0))$.
+
+    Args:
+      t: A vector of time steps.
+      sigma: The $\sigma$ in our SDE.
+
+    Returns:
+      The standard deviation.
+    """
+    t = torch.tensor(t, device=device)
+    return torch.sqrt((sigma ** (2 * t) - 1.) / 2. / np.log(sigma))
+
+
