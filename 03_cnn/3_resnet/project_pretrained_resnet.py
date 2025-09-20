4dbc7c8dc85069d52044d89552fcdd6253beee1b
wangrl2016 <wangrl2016@gmail.com>
2025-09-20 12:54:56 +0800

    Update content

--- 03_cnn/3_resnet/project_pretrained_resnet.py
+++ 03_cnn/3_resnet/project_pretrained_resnet.py
@@ -13,27 +13,31 @@ from common import download
 resnet = torchvision.models.resnet34(pretrained=True)
 resnet.eval()
 
-img_path = download.download('https://img-datasets.s3.amazonaws.com/cat.jpg')
-image = Image.open(img_path).convert('RGB')
+img_path = download.download("https://img-datasets.s3.amazonaws.com/cat.jpg")
+image = Image.open(img_path).convert("RGB")
 
-preprocess = torchvision.transforms.Compose([
-    torchvision.transforms.Resize(256),
-    torchvision.transforms.CenterCrop(224),
-    torchvision.transforms.ToTensor(),
-    torchvision.transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                     std=[0.229, 0.224, 0.225])
-])
+preprocess = torchvision.transforms.Compose(
+    [
+        torchvision.transforms.Resize(256),
+        torchvision.transforms.CenterCrop(224),
+        torchvision.transforms.ToTensor(),
+        torchvision.transforms.Normalize(
+            mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+        ),
+    ]
+)
 
 input_tensor = preprocess(image).unsqueeze(0)  # [1, 3, 224, 224]
 print(input_tensor.shape)
 
-pyplot.axis('off')
+pyplot.axis("off")
 pyplot.imshow(image)
 pyplot.subplots_adjust(left=0, right=1.0, top=1.0, bottom=0)
 pyplot.show()
 
 imagenet_labels_path = download.download(
-    "https://raw.githubusercontent.com/pytorch/hub/master/imagenet_classes.txt")
+    "https://raw.githubusercontent.com/pytorch/hub/master/imagenet_classes.txt"
+)
 with open(imagenet_labels_path) as f:
     labels = [line.strip() for line in f.readlines()]
 
